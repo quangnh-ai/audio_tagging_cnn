21357700a15b9364ed171c723686627911fb0101
unknown <quang009182@gmail.com>
2022-03-02 12:11:15 +0700

    update api respone: change field kf_id -> shot_id

--- utils/retrieval_model.py
+++ utils/retrieval_model.py
@@ -1,12 +1,13 @@
 import h5py
 from matplotlib.image import thumbnail
 import numpy as np
-
+import json
 import argparse
 
 def get_arg():
     parser = argparse.ArgumentParser()
     parser.add_argument('--features_path', type=str)
+    parser.add_argument('--mapping_kf2shot', type=str)
 
     args = parser.parse_args()
     return args
@@ -14,6 +15,9 @@ def get_arg():
 class Retrieval:
     def __init__(self, args):
         self.data = h5py.File(args.features_path, 'r')
+        
+        content = open(args.mapping_kf2shot)
+        self.mapping_dict = json.load(content)
 
         self.features = self.data.get('features')
         self.ids = self.data.get('shot_ids')
@@ -35,11 +39,13 @@ class Retrieval:
                 else:
                     dataset = 'V3C2'
                 keyframe_ids = self.ids[i]
+                # shot_id = self.mapping_dict.get(keyframe_ids)
                 thumbnail_path = dataset + '/' + video_id + '/' + keyframe_ids + '.png'
                 results.append({
                     "dataset": dataset,
                     "video_id": video_id,
-                    "frame_id": keyframe_ids,
+                    "shot_id": keyframe_ids,
+                    # "frame_id": keyframe_ids,
                     'thumbnail_path': thumbnail_path
                 })
         return results
