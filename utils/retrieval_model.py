c0727d37e92e8a741046a36e1fd6110f1b78b56f
unknown <quang009182@gmail.com>
2022-03-04 18:50:00 +0700

    reformat api response

--- utils/retrieval_model.py
+++ utils/retrieval_model.py
@@ -23,7 +23,8 @@ class Retrieval:
         self.mapping_values = list(self.mapping_dict.values())
 
         self.features = self.data.get('features')
-        self.ids = self.data.get('shot_ids')
+        self.shot_ids = self.data.get('shot_ids')
+        self.keyframe_ids = self.data.get('keyframe_ids')
         
         self.ids = [id.decode("utf-8") for id in self.ids]
         self.features = [feature.decode("utf-8") for feature in self.features]
@@ -40,20 +41,21 @@ class Retrieval:
         for i in range(len(self.features)):
             if query in self.features[i]:
                 # results.append(self.ids[i])
-                video_id = self.ids[i].split('_')[0]
+                video_id = self.shot_ids[i].split('_')[0]
                 dataset = None
                 if int(video_id) <= 7475:
                     dataset = 'V3C1'
                 else:
                     dataset = 'V3C2'
-                shot_id = self.ids[i]
-                keyframe_id = self.get_keyframe_id(shot_id)
+                shot_id = self.shot_ids[i].split('_')[1]
+                keyframe_id = self.keyframe_ids[i].split('_')[1]
+                # keyframe_id = self.get_keyframe_id(shot_id)
                 thumbnail_path = dataset + '/' + video_id + '/' + shot_id + '/' + video_id + '_' + shot_id + '_' + keyframe_id + '.jpg'
                 results.append({
                     "dataset": dataset,
                     "video_id": video_id,
-                    "shot_id": shot_id,
-                    "frame_id": keyframe_id,
+                    "shot_id": int(shot_id),
+                    "frame_id": int(keyframe_id),
                     'thumbnail_path': thumbnail_path
                 })
         return results
